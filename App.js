dee6640a6ae0c85004fef3cb19da48035250cab9
shako metonidze <120588118+shakomentol@users.noreply.github.com>
2023-02-01 00:28:03 -0800

    update app.js

--- App.js
+++ App.js
@@ -21,3 +21,14 @@ for (let index = 1; index < 1000; length; index++) {
     numbers.push(index * index);
 }
 console.log(numbers);
+
+const names = ["salome","shako"];
+for (let index = 0; index < names.length; index++) {
+    if (names[index].length > 2) {
+      const element = names[index].toUpperCase(); 
+    }
+    console.log(element);
+else {
+    console.log("wrong act");
+}
+}
