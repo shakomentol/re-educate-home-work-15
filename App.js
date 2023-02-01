0e77621ab4feaba2d8d4e478a80b76ee23ae7841
shako metonidze <120588118+shakomentol@users.noreply.github.com>
2023-01-31 23:43:44 -0800

    create app.js

---
+++ App.js
@@ -0,0 +1,23 @@
+let sum = 0;
+for (let i = 0; i < 100 ; i++) {
+   console.log (i); 
+}
+
+let w = 0;
+while (w < 50) {
+    i++;
+    console.log (w);
+}
+
+let person = ["salome", "shako", "diana", "giorgi", "damiane"];
+person.pop();
+person.push("irakli");
+person.shift();
+person.unshift("mariami");
+console.log (person);
+
+let numbers = [];
+for (let index = 1; index < 1000; length; index++) {
+    numbers.push(index * index);
+}
+console.log(numbers);
